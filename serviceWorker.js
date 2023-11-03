8483cb70b6357009cc3db4d4fe4d8e3bd3985f9e
Imamudin Naseem <naseem1992@gmail.com>
2023-11-03 16:52:23 +0530

    update manifest

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const storymap = "storymap"
+const storymap = "storymap-v1"
 const assets = ["/", "/index.html", "/how-to-use.html", '/images/icons/icon-48x48.png', '/images/icons/icon-72x72.png', '/images/icons/icon-96x96.png', '/images/icons/icon-128x128.png', '/images/icons/icon-144x144.png', '/images/icons/icon-152x152.png', '/images/icons/icon-192x192.png', '/images/icons/icon-384x384.png', '/images/logo-grey.png',]
 
 self.addEventListener("install", installEvent => {
