c9f28f2e190e64ab2210364785e58aefc04bad76
Imamudin Naseem <naseem1992@gmail.com>
2023-11-03 17:01:36 +0530

    update sw version

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-var cacheName = 'v1';
+var cacheName = 'v2';
 
 // Default files to always cache
 var cacheFiles =["/", "/index.html", "/how-to-use.html", '/images/icons/icon-48x48.png', '/images/icons/icon-72x72.png', '/images/icons/icon-96x96.png', '/images/icons/icon-128x128.png', '/images/icons/icon-144x144.png', '/images/icons/icon-152x152.png', '/images/icons/icon-192x192.png', '/images/icons/icon-384x384.png', '/images/logo-grey.png',]
